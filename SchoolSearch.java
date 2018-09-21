c5c74e6095a28c79ed4ed91e0b0807c9e839f3b1
Abhinav Singh <abhinav.s1997@yahoo.com>
2018-09-21 15:58:19 -0700

    Add files via upload

--- SchoolSearch.java
+++ SchoolSearch.java
@@ -2,20 +2,46 @@ import java.util.*;
 
 public class SchoolSearch{
 
+   ArrayList<StudentObj> students = new ArrayList<StudentObj>();
+
    public static void main(String[] args){
+      populateStudents();
       startUp();
    }
 
+   public static void populateStudents(){
+
+   }
+   
    public static void startUp(){
       String opitions = "Here are your opitions:\n S[tudent]: <lastname> [B[us]]\n T[eacher]: <lastname>\n" + 
                         "B[us]: <number>\n G[rade]: <number> [[H[igh]] | [L[ow]]\n A[verage]: <number> " + 
                         "I[nfo]\n Q[uit]";
       System.out.println(opitions);
+
       Scanner scan = new Scanner(System.in);
-      while(!scan.next().equals("Q")){
-         System.out.println("Testing");
+      String command = scan.next();
+      
+      while(!command.equals("Q")){
+         String [] strSplit = command.split(":");
+         String opition = strSplit[0];
+         switch(opition){
+            case "S":
+               String argument = strSplit[1];
+               boolean bus = false;
+               if(argument.getCharAt(argument.length()-1) == 'B'){
+                  bus = true;
+               }
+               studentSearch(argument.substring(0, arugment.length()-1), bus);
+               break;
+         }
+         command = scan.next();
       }
 
       System.out.println("Good Bye");
+   }
+
+   public static void studentSearch(String argument, boolean bus){
+      while()
    }
 }
